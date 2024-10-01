c1e23511ac396c3b1ab8e77f6302374d18aff1de
Adrian H <adrianhoff@gmail.com>
2024-10-01 12:14:52 +0200

    fix post

--- WebMVC/obj/Debug/net8.0/WebMVC.AssemblyInfo.cs
+++ WebMVC/obj/Debug/net8.0/WebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9592121ea60f7424173a240c7d900a78f011b43b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baf234f94bc8ceca333a580dfb24499b01bf18da")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
