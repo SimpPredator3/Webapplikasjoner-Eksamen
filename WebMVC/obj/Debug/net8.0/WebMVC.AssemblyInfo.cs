d90f4e892b809a641edf8160fa7f88ec885bc7ba
SimpPredator3 <114159305+SimpPredator3@users.noreply.github.com>
2024-10-01 12:08:49 +0200

    fjerna enda en s

--- WebMVC/obj/Debug/net8.0/WebMVC.AssemblyInfo.cs
+++ WebMVC/obj/Debug/net8.0/WebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dc07c800dbe71817f9ae535e8df99dd3c40929d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c21b543184ae7e24e7ee16577fff361dc11311b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
