2e235b4f1b786bc92ff3063a76db7a42b43cda77
LightningGoose <115466765+LightningGoose@users.noreply.github.com>
2024-10-09 11:39:53 +0200

    merge av utvikling

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ KodeGjengen
 ### Oversikt over hva hver enkelt jobber
 Dette er en oversikt over alle medlemmer i prosjektet og hva de jobber med for øyeblikket. Hver person har sitt navn oppført, etterfulgt av en beskrivelse av deres nåværende oppgave. Listen oppdateres fortløpende for å holde teamet informert om pågående arbeid. 
 <br> **Sjekk denne før man begynner på noe nytt på siden, da slipper vi å jobbe dobbelt.**
-* Ask - Jobbe med Forsiden.
-* Adrian - Brukere og logg inn, med passord og brukernavn. lager noen design layouts. redigere smått med HomeView
-* Martin - Jobber med Posts og DB.
-* Rick - Jobbe med å legge til kommentar-funksjonalitet.
+* Ask -
+* Adrian - lager noen design layouts. redigere smått med HomeView
+* Martin -
+* Rick -
 
 ------------------------------------------------------------------
 
---
+++ WebMVC/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.EntityFrameworkCore;
+using WebMVC.DAL;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Retrieve the connection string for the ApplicationDbContext
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Register ApplicationDbContext with the correct connection string
+builder.Services.AddDbContext<ApplicationDbContext>(options => {
+    options.UseSqlite(connectionString); // Using the correct connection string
+});
+
+
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Register the PostRepository for Dependency Injection
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+
+builder.Services.AddRazorPages();
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+app.Run();
