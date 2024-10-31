ebb97100042a9925b93d20587c53d9b9ecd06337
izRickB <78593114+izRickB@users.noreply.github.com>
2024-10-31 12:24:40 +0100

    Update CommentRepository.cs

--- WebMVC/DAL/CommentRepository.cs
+++ WebMVC/DAL/CommentRepository.cs
@@ -1,8 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using WebMVC.Models;
 using Microsoft.EntityFrameworkCore;
+using WebMVC.Models;
 
 namespace WebMVC.DAL
 {
@@ -15,30 +15,35 @@ namespace WebMVC.DAL
             _context = context;
         }
 
+        // Get a single comment by ID
         public async Task<Comment?> GetCommentByIdAsync(int id)
         {
             return await _context.Comments.FirstOrDefaultAsync(c => c.Id == id);
         }
 
-        public async Task<IEnumerable<Comment>> GetCommentsByPostIdAsync(int postId)
+        // Get all comments for a specific post
+        public async Task<List<Comment>> GetCommentsByPostIdAsync(int postId)
         {
             return await _context.Comments
                 .Where(c => c.PostId == postId)
                 .ToListAsync();
         }
 
+        // Add a new comment
         public async Task<bool> AddCommentAsync(Comment comment)
         {
             _context.Comments.Add(comment);
             return await _context.SaveChangesAsync() > 0;
         }
 
+        // Update an existing comment
         public async Task<bool> UpdateCommentAsync(Comment comment)
         {
             _context.Comments.Update(comment);
             return await _context.SaveChangesAsync() > 0;
         }
 
+        // Delete a comment, checking that the author matches or user is an admin
         public async Task<bool> DeleteCommentAsync(int id, string userId)
         {
             var comment = await GetCommentByIdAsync(id);
